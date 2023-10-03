7f4d3308e4d2010ad70cade6dfcfa085cdd49674
尛柒诶 <132661400+JustinSparrrow@users.noreply.github.com>
2023-10-04 04:44:34 +0800

    Add files via upload

---
+++ leetcode/leetcode/easy/14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    string longestCommonPrefix(vector<string>& strs){
+        vector<vector<string> >s;
+        for(int i = 0; i < strs.size();i++){
+                if(strs[i]>='a'&&strs[i]<='z')
+
+        }
+    }
+};
