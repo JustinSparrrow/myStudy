7ca7575388b1746845fc16876d061454ae121cc4
尛柒诶 <132661400+JustinSparrrow@users.noreply.github.com>
2023-10-06 00:21:30 +0800

    Create 21.cpp

---
+++ leetcode/leetcode/easy/21.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+int cmp(ListNode* a,ListNode* b)
+{
+    return a->val<b->val;
+}
+class Solution{
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        if(!list1)  return list2;
+        if(!list2)  return list1;
+
+        vector<ListNode*>n;
+
+        while (list1){
+            n.push_back(list1);
+            list1=list1->next;
+        }
+        while (list2){
+            n.push_back(list2);
+            list2=list2->next;
+        }
+        sort(n.begin(),n.end(), cmp);
+        //test
+//        for(int i = 0; i< n.size();i++)
+//            cout << n[i];
+        //end
+        ListNode* list3 = new ListNode(n[0]->val);
+        ListNode* current;
+        ListNode* follow = list3;
+        for(int i = 1; i < n.size(); i++){
+            current = new ListNode(n[i]->val);
+            follow->next=current;
+            follow = current;
+        }
+        return list3;
+    }
+};
