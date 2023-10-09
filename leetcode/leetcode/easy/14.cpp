cfdf7b75e86245cece1b02ff57815c13c82c0e45
尛柒诶 <132661400+JustinSparrrow@users.noreply.github.com>
2023-10-09 12:15:11 +0800

    Update 14.cpp

--- leetcode/leetcode/easy/14.cpp
+++ leetcode/leetcode/easy/14.cpp
@@ -6,10 +6,19 @@ using namespace std;
 class Solution{
 public:
     string longestCommonPrefix(vector<string>& strs){
-        vector<vector<string> >s;
-        for(int i = 0; i < strs.size();i++){
-                if(strs[i]>='a'&&strs[i]<='z')
-
+        if(!strs.size()) return "";
+        string prefix = strs[0];
+        for(int i = 1; i < strs.size(); i++){
+            prefix = stringCompare(prefix, strs[i]);
+            if(!prefix.size()) break;
         }
+        return prefix;
+    }
+
+    string stringCompare(string &str1, string &str2){
+        int length = min(str1.size(), str2.size());
+        int index = 0;
+        while (index < length && str1[index] == str2[index]) index++;
+        return str1.substr(0,index);
     }
 };
